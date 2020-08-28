4d64af73f90f45ab442fcd884d00bcf2f225e9ca
DatPT <phamtiendatkhanh@gmail.com>
2020-08-28 14:28:06 +0700

    change port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app'
 import dotenv from 'dotenv'
 dotenv.config()
 
-app.listen(process.env.PORT || 9002, () => {
-  console.log('Server started on port: ', process.env.PORT || 9002);
+app.listen(process.env.PORT || 9007, () => {
+  console.log('Server started on port: ', process.env.PORT || 9007);
 })
